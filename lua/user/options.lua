5a771dd2863a691cc249a96da85d4e5527a965cd
Augusto Areias <areiass36@gmail.com>
2024-03-12 21:05:37 -0300

    First config files

---
+++ lua/user/options.lua
@@ -0,0 +1,12 @@
+local options = {
+	clipboard = "unnamedplus",
+	number = true,
+	relativenumber = true,
+	tabstop = 4,							-- set tab size
+	wildmenu = true,
+	wildmode = "list:longest"
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
