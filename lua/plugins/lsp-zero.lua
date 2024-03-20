0eb8ab0743aa0236c3cf7502beeb98fc047b5917
Augusto Areias <areiass36@gmail.com>
2024-03-20 00:15:31 -0300

    Plugin manager changes

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -8,37 +8,37 @@ return {
 			lsp_zero.on_attach(function(client, bufnr)
 				lsp_zero.default_keymaps({ buffer = bufnr })
 			end)
-			require('lspconfig').csharp_ls.setup({})
+
+			require('mason').setup({})
+			require('mason-lspconfig').setup({
+				-- Replace the language servers listed here
+				-- with the ones you want to install
+				ensure_installed = { 'volar' },
+				handlers = {
+					lsp_zero.default_setup,
+				vuels = lsp_zero.noop,
+
+				},
+			})
+			--			require('lspconfig').csharp_ls.setup({})
 			--			require('lspconfig').tsserver.setup({})
 			require('lspconfig').lua_ls.setup({})
-			require('lspconfig').volar.setup({
-				filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
-				init_options = {
-					languageFeatures = {
-						implementation = true,
-						references = true,
-						definition = true,
-						typeDefinition = true,
-						callHierarchy = true,
-						hover = true,
-						rename = true,
-						renameFileRefactoring = true,
-						signatureHelp = true,
-						codeAction = true,
-						workspaceSymbol = true,
-						completion = {
-							defaultTagNameCase = 'kebabCase',
-							defaultAttrNameCase = 'kebabCase',
-							getDocumentNameCasesRequest = false,
-							getDocumentSelectionRequest = false,
-						},
-					},
-				}
-			})
+			require('lspconfig').volar.setup({})
+			--				filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+			--				init_options = {
+			--					typescript = {
+			--						tsdk = '/path/to/.npm/lib/node_modules/typescript/lib'
+			-- Alternative location if installed as root:
+			--						-- tsdk = '/usr/local/lib/node_modules/typescript/lib'
+			--					}
+			--				}
+			--			})
 		end,
 	},
 	{ 'neovim/nvim-lspconfig' },
 	{ 'hrsh7th/cmp-nvim-lsp' },
 	{ 'hrsh7th/nvim-cmp' },
 	{ 'L3MON4D3/LuaSnip' },
+	{ "williamboman/mason.nvim", },
+	{ "williamboman/mason-lspconfig.nvim", }
 }
