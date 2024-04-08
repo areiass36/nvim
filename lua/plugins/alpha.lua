d910f4a3fffd25270defb2aee52b556df8ee82b7
Augusto Areias <areiass36@gmail.com>
2024-04-08 19:45:25 -0300

    Added init login page

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,43 @@
+return {
+	'goolord/alpha-nvim',
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
+
+		_Gopts = {
+			position = "center",
+			hl = "Type",
+		}
+
+		local function footer()
+			return "areiass36"
+		end
+		dashboard.section.footer.val = footer()
+
+		dashboard.opts.opts.noautocmd = true
+		alpha.setup(dashboard.opts)
+
+		require("alpha").setup(dashboard.opts)
+	end,
+}
