ffe74d106575f19a7cc57ba304f80a9faaac4414
Augusto Areias <areiass36@gmail.com>
2024-04-12 00:26:29 -0300

    Changed Csharp lsp to omnisharp

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -19,11 +19,10 @@ return {
 			-- mason lsp installer
 			require('mason').setup({})
 			require('mason-lspconfig').setup({
-				ensure_installed = { 'volar', 'tsserver', 'csharp_ls', 'lua_ls', 'pylsp', 'angularls' },
+				ensure_installed = { 'volar', 'tsserver', 'lua_ls', 'pylsp', 'angularls', 'omnisharp' },
 				handlers = {
 					lsp_zero.default_setup,
 					vuels = lsp_zero.noop,
-
 				},
 			})
 
@@ -32,7 +31,36 @@ return {
 			local macNodePath = "/Users/areiass36/.asdf/installs/nodejs/20.11.1/lib/node_modules"
 
 			local npmPath = os == "Darwin" and macNodePath or windowsNodePath
-			lspconfig.csharp_ls.setup({})
+			--	lspconfig.csharp_ls.setup({
+			--		config = {
+			--			handlers = {
+			--				["textDocument/definition"] = require('csharpls_extended').handler,
+			--				["textDocument/typeDefinition"] = require('csharpls_extended').handler,
+			--			},
+			--					cmd = { csharpls },
+			--			cmd = { "dotnet", "/Users/areiass36/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+			--			}
+			--		})
+			lspconfig.omnisharp.setup({
+				cmd = { "dotnet", "/Users/areiass36/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+				settings = {
+					FormattingOptions = {
+						VarForBuiltInType = true,
+						OrganizaImports = true
+					},
+					RoslynExtensionsOptions = {
+						EnableAnalyzerSupport = nil
+					}
+				},
+				handlers = {
+					["textDocument/definition"] = require('omnisharp_extended').definition_handler,
+					["textDocument/typeDefinition"] = require('omnisharp_extended')
+					    .type_definition_handler,
+					["textDocument/references"] = require('omnisharp_extended').references_handler,
+					["textDocument/implementation"] = require('omnisharp_extended')
+					    .implementation_handler,
+				}
+			})
 			lspconfig.lua_ls.setup({
 				settings = {
 					Lua = {
@@ -70,10 +98,12 @@ return {
 			})
 		end,
 	},
+	{ 'Decodetalkers/csharpls-extended-lsp.nvim' },
+	{ 'Hoffs/omnisharp-extended-lsp.nvim' },
 	{ 'neovim/nvim-lspconfig' },
 	{ 'hrsh7th/cmp-nvim-lsp' },
 	{ 'hrsh7th/nvim-cmp' },
 	{ 'L3MON4D3/LuaSnip' },
 	{ "williamboman/mason.nvim", },
-	{ "williamboman/mason-lspconfig.nvim", }
+	{ "williamboman/mason-lspconfig.nvim", },
 }
